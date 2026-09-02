c460ae2b38f1ecd5c1a131eb7cebfd3bc21a60f7
vtu28991 <vtu28991@veltech.edu.in>
2026-09-02 13:58:04 +0530

    Time: 32 ms (77.61%), Space: 100.1 MB (43.29%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,37 +1,30 @@
-import java.util.Stack;
-
 class MinStack {
-
-    Stack<Integer> stack;
-    Stack<Integer> minStack;
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
 
     public MinStack() {
         stack = new Stack<>();
         minStack = new Stack<>();
     }
-
+    
     public void push(int val) {
-
         stack.push(val);
-
         if (minStack.isEmpty() || val <= minStack.peek()) {
             minStack.push(val);
+        } else {
+            minStack.push(minStack.peek());
         }
     }
-
+    
     public void pop() {
-
-        if (stack.peek().equals(minStack.peek())) {
-            minStack.pop();
-        }
-
         stack.pop();
+        minStack.pop();
     }
-
+    
     public int top() {
         return stack.peek();
     }
-
+    
     public int getMin() {
         return minStack.peek();
     }
